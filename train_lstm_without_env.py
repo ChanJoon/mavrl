3a9fd503db8437fa83a2e40b3c697bb43c521fc2
NPU-yuhang <h.y.yu@tudelft.nl>
2024-02-15 19:00:57 +0000

    update

--- train_lstm_without_env.py
+++ train_lstm_without_env.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import argparse
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 from os.path import join, exists
 import torch
+import numpy as np
 from stable_baselines3.common.utils import get_device
 from mav_baselines.torch.recurrent_ppo.policies import MultiInputLstmPolicy
 from mav_baselines.torch.recurrent_ppo.ppo_recurrent import RecurrentPPO
+
+def configure_random_seed(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
 def parser():
     parser = argparse.ArgumentParser()
@@ -23,14 +28,15 @@ def parser():
 
 def main():
     args = parser().parse_args()
-
+    configure_random_seed(92)
     rsg_root = os.path.dirname(os.path.abspath(__file__))
     log_dir = rsg_root + "/saved"
+    print(log_dir)
     device = get_device("auto")
+    print(device)
     weight = os.environ["AVOIDBENCH_PATH"] + "/../mavrl/saved/RecurrentPPO_{0}/Policy/iter_{1:05d}.pth".format(args.trial, args.iter)
-    device = get_device("auto")
+    print(weight)
     saved_variables = torch.load(weight, map_location=device)
-    # print(saved_variables["state_dict"])
     # Create policy object
     saved_variables["data"]['only_lstm_training'] = True
     policy = MultiInputLstmPolicy(features_dim=64, 
@@ -41,38 +47,40 @@ def main():
     policy.load_state_dict(saved_variables["state_dict"], strict=False)
     policy.to(device)
     print(args.recon)
+    model = RecurrentPPO(
+        tensorboard_log=log_dir,
+        policy=policy,
+        policy_kwargs=dict(
+        activation_fn=torch.nn.ReLU,
+        net_arch=[dict(pi=[256, 256], vf=[512, 512])],
+        ),
+        use_tanh_act=True,
+        gae_lambda=0.95,
+        gamma=0.99,
+        n_steps=1000,
+        n_seq=1,
+        ent_coef=0.0,
+        vf_coef=0.5,
+        max_grad_norm=0.5,
+        lstm_layer=1,
+        batch_size=500,
+        n_epochs=2000,
+        clip_range=0.2,
+        use_sde=False,  # don't use (gSDE), doesn't work
+        retrain=args.retrain,
+        verbose=1,
+        only_lstm_training=True,
+        states_dim=0,
+        reconstruction_members=args.recon,
+        reconstruction_steps=10,
+        train_lstm_without_env=True,
+        lstm_dataset_path=args.dir,
+        lstm_weight_saved_path=args.lstm_exp,
+    )
     if args.train:
-        model = RecurrentPPO(
-            tensorboard_log=log_dir,
-            policy=policy,
-            policy_kwargs=dict(
-            activation_fn=torch.nn.ReLU,
-            net_arch=[dict(pi=[256, 256], vf=[512, 512])],
-            ),
-            use_tanh_act=True,
-            gae_lambda=0.95,
-            gamma=0.99,
-            n_steps=1000,
-            n_seq=1,
-            ent_coef=0.0,
-            vf_coef=0.5,
-            max_grad_norm=0.5,
-            lstm_layer=1,
-            batch_size=500,
-            n_epochs=2000,
-            clip_range=0.2,
-            use_sde=False,  # don't use (gSDE), doesn't work
-            retrain=args.retrain,
-            verbose=1,
-            only_lstm_training=True,
-            states_dim=0,
-            reconstruction_members=args.recon,
-            reconstruction_steps=10,
-            train_lstm_without_env=True,
-            lstm_dataset_path=args.dir,
-            lstm_weight_saved_path=args.lstm_exp,
-        )
         model.train_lstm_from_dataset()
+    else:
+        model.test_lstm_seperate()
 
 if __name__ == "__main__":
     main()
